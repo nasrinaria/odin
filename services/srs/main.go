6beb95c6a067700a9204dbcb70171161d5e40d72
nasrinaria <nasrin.ariafar@gmail.com>
2025-09-25 11:53:47 +0200

    Added health endpoint for srs service

--- services/srs/main.go
+++ services/srs/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello, SRS Service!")
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.Writer.Write([]byte("OK"))
+	})
+	router.Run(":8080")
 }
